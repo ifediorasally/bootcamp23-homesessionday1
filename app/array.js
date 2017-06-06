70c28a8247625b84e90a9f05793f9b4772d6c187
unknown <ifediorasally@gmail.com>
2017-06-06 16:24:48 +0100

    arithmetic and geometric progression for an array of numbers

---
+++ app/array.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+    
+aritGeo: function(arr) {
+    var ap, gp;
+    for (var i=0; i< (arr.length - 2); i++)
+    if (!(ap = arr [i+1] - arr [i] == arr [i+2] - arr [i+1])) break;
+
+    if (ap) return "Arithmetic";
+
+    for (i = 0; i < (arr.length - 2); i++)
+    if (!(gp = arr [i+1] / arr [i] == arr [i+2] / arr [i+1])) break;
+
+    if (gp) return "Geometric";
+
+    return -1;
+    }
+}
